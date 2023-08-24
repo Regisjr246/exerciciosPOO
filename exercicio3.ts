49abd1add7b9cb5c0702195b4ae0674137f7f5c0
Regisjr246 <regisjr246@gmail.com>
2023-08-24 15:48:38 -0300

    exercicio 3

---
+++ exercicio3.ts
@@ -0,0 +1,48 @@
+class Estudante{
+    nome: string;
+    idade:number;
+    notas:number[];
+ 
+    
+
+
+
+constructor(nome:string, idade:number, notas:number[]){
+
+this.nome= nome;
+this.idade = idade;
+this.notas= notas;
+
+}
+
+
+Media(){
+
+let somaNotas=0;
+
+
+
+    for(let i=0;  i<this.notas.length; i++){
+        somaNotas= somaNotas+this.notas[i];    
+    }
+
+    let media= somaNotas/this.notas.length
+
+if(media>7){
+    return "Parabéns " + this.nome   + " sua média é " + media + " e você está aprovado(a)"
+} else {
+    return this.nome +", sua média é " + media +" e você precisa estudar mais para melhorar seu desempenho"
+}
+
+
+}
+
+
+
+
+
+
+}
+
+let julia = new Estudante("Maria",20, [8, 7.5, 9.2, 6.8, 9.5]);
+console.log(julia.Media());
