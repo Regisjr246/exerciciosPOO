6b5b13321b642399ccf379138f9ec2febc532d32
Regisjr246 <regisjr246@gmail.com>
2023-08-24 14:00:49 -0300

    exericios

---
+++ exercicio2.ts
@@ -0,0 +1,81 @@
+class ContaBancaria {
+
+    titular: string;
+    saldo: number;
+
+
+
+    constructor(titular: string, saldo: number) {
+
+        this.titular = titular;
+        this.saldo = saldo
+
+    }
+
+
+
+
+    Saldo() {
+
+        return " A conta bancária chamada: minhaConta com o titular:" + this.titular + " contendo um saldo inicial de R$" + this.saldo + " foi criada com sucesso"
+
+    }
+
+
+    deposito(deposito: number) {
+this.saldo= this.saldo+deposito
+
+        return "O depósito de R$" + deposito + " foi realizado com sucesso, agora seu saldo atual é " + this.saldo + " reais"
+
+    }
+
+
+
+    saque(saque: number) {
+
+        this.saldo=this.saldo - saque
+        if (saque>this.saldo){
+            return "Saque Negado, seu saldo é de " + (this.saldo+saque)
+        } else
+        { return "Saque realizado, agora seu saldo é R$ " + this.saldo} 
+
+
+
+
+       
+}
+    
+
+
+
+
+
+
+}
+
+
+let joao = new ContaBancaria("Joao", 1000);
+let thorss =new ContaBancaria("Thorss", 2000);
+let eduardoMecanico =new ContaBancaria("Eduardo Mecanico", 2000);
+
+
+/*João*/
+console.log(joao.Saldo());
+console.log(joao.deposito(500));
+console.log(joao.saque(200));
+console.log(joao.saque(100000));
+
+/*Thorss*/
+console.log("");
+console.log(thorss.Saldo());
+console.log(thorss.deposito(500));
+console.log(thorss.saque(200));
+console.log(thorss.saque(100000));
+
+
+/*Eduardo Mecanico*/
+console.log("");
+console.log(eduardoMecanico.Saldo());
+console.log(eduardoMecanico.deposito(500));
+console.log(eduardoMecanico.saque(200));
+console.log(eduardoMecanico.saque(100000));
